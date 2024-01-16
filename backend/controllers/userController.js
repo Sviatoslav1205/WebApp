45d96981fd71e19d26bd3642be96e1798000a8c5
Sviatoslav Yemets <svjatoslav.yemets@gmail.com>
2024-01-17 00:37:50 +0200

    jwt test

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,12 @@
 const { pool } = require('../db/db')
 
 const getUserByUserId = async (userId) => {
-  pool.query("SELECT * FROM `Users` WHERE `user_id` = ?", userId, (err, res) => {
-    
+  pool.query("SELECT * FROM `Users` WHERE `user_id` = ?", [userId], (error, result) => {
+    if (error) {
+      console.log(error)
+      // throw error
+    }
+    return "ffff"
   })
 }
 
--- backend/routes/jwtToken.js
+++ backend/routes/jwtToken.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const userController = require('../controllers/userController')
 
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
   // console.log(req.baseUrl)
   const { userId } = req.body
-  const user = await getUserByUserId(userId)
+  const user = await userController.getUserByUserId(userId)
+
+  console.log(user)
 
   res.json({success: true})
 })
