99deb1b1c51389a8a7ba4156512f10f6f89e8895
Sviatoslav Yemets <svjatoslav.yemets@gmail.com>
2024-01-21 17:35:59 +0200

    added user role middleware

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,7 +1,9 @@
 const adminController = require('../controllers/admin.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
+const userRoleMiddleware = require('../middlewares/userRole.middleware')
 const router = require('express').Router()
 
-router.post('/generate-password/:userId', authMiddleware, adminController.generatePassword)
+// router.post('/generate-password/:userId', authMiddleware, adminController.generatePassword)
+router.post('/generate-password/:userId', userRoleMiddleware('admin'), adminController.generatePassword)
 
 module.exports = router
