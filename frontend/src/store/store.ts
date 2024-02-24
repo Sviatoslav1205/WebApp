c45c47cc74d16b884d946c294a4203093ef10bb3
Sviatoslav Yemets <svjatoslav.yemets@gmail.com>
2024-02-24 18:50:27 +0200

    .

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -44,7 +44,7 @@ export default class Store {
 
   async logout() {
     try {
-      const response = await AuthService.logout()
+      await AuthService.logout()
       localStorage.removeItem('accessToken')
       this.setAuth(false)
       this.setUser({} as IUser)
